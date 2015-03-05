f6cd7890cf2708e3860e22e48401044b78fa5006
markwynter <mark@dimensionaledge.com>
2015-03-05 02:47:06 +0000

    added

---
+++ lines/DE_BezierCurve2D.sql
@@ -0,0 +1,86 @@
+---------------------------------------------------------------------------
+-- Code Desciption:
+---------------------------------------------------------------------------
+-- PostgreSQL/PostGIS custom function for generating 2D bezier curves.
+-- Dependencies: None
+-- Developed by: mark[a]dimensionaledge[dot]com
+-- Licence: GNU GPL version 3.0
+---------------------------------------------------------------------------
+
+DROP FUNCTION IF EXISTS DE_BezierCurve2D(geometry, geometry, numeric, integer);
+CREATE OR REPLACE FUNCTION DE_BezierCurve2D(o_geom geometry, d_geom geometry, theta numeric, num_points integer, breakval numeric)
+RETURNS SETOF geometry AS
+$$
+DECLARE
+srid INTEGER DEFAULT 4326;
+endval INTEGER;
+o_x float8;
+o_y float8;
+d_x float8;
+d_y float8;
+base_len float8;
+hyp_len float8;
+az_deg float8;
+theta_adj float8;
+a_geom GEOMETRY;
+a_x float8;
+a_y float8;
+
+BEGIN
+endval := num_points-1;
+
+--GET COORDINATE X-Y VALUES
+o_geom := ST_Transform(o_geom, srid);
+d_geom := ST_Transform(d_geom, srid);
+o_x := ST_X(o_geom);
+o_y := ST_Y(o_geom);
+d_x := ST_X(d_geom);
+d_y := ST_Y(d_geom);
+
+--TRANSLATE X-COORDINATES TO 0-360 DEGREE SCALE
+o_x := CASE WHEN o_x < 0 THEN o_x + 360 ELSE o_x END;
+d_x := CASE WHEN d_x < 0 THEN d_x + 360 ELSE d_x END;
+
+--CALCULATE AZIMUTH FROM TRUE NORTH
+az_deg := degrees(ST_Azimuth(ST_Point(o_x, o_y), ST_Point(d_x, d_y)));
+
+--CALCULATE LENGTH OF TRIANGULAR BASE
+base_len :=
+	CASE WHEN abs(d_x - o_x) <= 180 THEN (((d_x - o_x)^2 + (d_y - o_y)^2)^0.5) * 0.5
+	ELSE (((360 - (d_x - o_x))^2 + (d_y - o_y)^2)^0.5) * 0.5
+	END;
+
+--ADJUST THETA (MAXIMUM ANGLE BETWEEN ADJACENT AND HYPOTENUSE. ZERO THETA IN NORTH-SOUTH PLANE.  MAXIMUM THETA IN EAST-WEST PLANE
+theta_adj :=
+	CASE WHEN az_deg >= 180 THEN -1 * theta * SIN(ST_Azimuth(ST_Point(o_x, o_y), ST_Point(d_x, d_y)))
+	ELSE theta * SIN(ST_Azimuth(ST_Point(o_x, o_y), ST_Point(d_x, d_y)))
+	END;
+
+--CALCULATE LENGTH OF HYPOTENUSE USING THETA ADJUSTED
+hyp_len := base_len/(COS(RADIANS(theta_adj)));
+
+--CALCULATE BEZIER APEX GEOMETRY
+a_geom := ST_Translate(ST_Point(o_x, o_y), SIN(RADIANS(az_deg - theta_adj))*hyp_len, COS(RADIANS(az_deg - theta_adj))*hyp_len);
+a_x := ST_X(a_geom);
+a_y := ST_Y(a_geom);
+
+--GENERATE BEZIER CURVE LINESTRING
+RETURN QUERY WITH 
+--generate required number of points
+s1 AS (SELECT s0/endval::numeric as p_value FROM generate_series(0,endval,1) s0),
+--generate the bezier curve x-y values
+s2 AS (SELECT p_value,
+(((1 - p_value)*(((1 - p_value)*o_x) + (p_value * a_x))) + (p_value * (((1 - p_value)*a_x) + (p_value * d_x)))) as C_x,
+(((1 - p_value)*(((1 - p_value)*o_y) + (p_value * a_y))) + (p_value * (((1 - p_value)*a_y) + (p_value * d_y)))) as C_y
+FROM s1),
+--group points to facilitate the creation of split linestrings, plus adjust back to -180 to 180 degree x-scale
+s3 AS (SELECT p_value, CASE WHEN C_x >= breakval THEN 1 ELSE 2 END as linenum, CASE WHEN C_x >= 180 THEN C_x - 360 ELSE C_x END as C_x, C_y FROM s2),
+--make point geometries
+s4 AS (SELECT p_value, linenum, ST_MakePoint(C_x, C_y) as wkb_geometry FROM s3),
+--make individual line geometries for line numbers 1 and 2
+s5 AS (SELECT 1 as foo, ST_SetSRID(ST_MakeLine(wkb_geometry ORDER BY p_value),srid) as wkb_geometry FROM s4 WHERE linenum = 1 UNION ALL SELECT 1, ST_SetSRID(ST_MakeLine(wkb_geometry ORDER BY p_value),srid) as wkb_geometry FROM s4 WHERE linenum = 2)
+--Return the multilinestring
+SELECT ST_Multi(ST_Union(wkb_geometry)) FROM s5 WHERE wkb_geometry IS NOT NULL GROUP BY foo;		
+
+END;
+$$ LANGUAGE plpgsql STRICT;
---
+++ tutorials/bezier_curve_flight_paths.sh
@@ -0,0 +1,126 @@
+#!/bin/bash
+# Bezier Curve Flight Paths Tutorial
+# Written by: mark[at]dimensionaledge[dot]com
+# Licence: GNU GPL version 3.0
+# Start timer
+START_T1=$(date +%s)
+# DB Connection Settings
+dbsettings="/mnt/data/common/repos/cf_private/settings/current.sh"
+export dbsettings
+source $dbsettings
+
+#######################################
+######### DATA PREPARATION ##########
+#######################################
+# bash function for downloading and ingesting tutorial data into PostgreSQL
+fn_getdata() {
+wget "http://www.mappinghacks.com/data/TM_WORLD_BORDERS_SIMPL-0.2.zip"
+unzip TM_WORLD_BORDERS_SIMPL-0.2.zip
+ogr2ogr -f "PostgreSQL" PG:"host=$host user=$username password=$password dbname=$dbname" $i -nln world_borders -s_srs EPSG:4326 -t_srs EPSG:4326 -a_srs EPSG:4326 -nlt MULTIPOLYGON
+}
+# end of bash function
+# call the bash function or comment # to skip
+fn_getdata 
+
+# get sql custom functions
+wget https://raw.githubusercontent.com/dimensionaledge/cf_public/master/lines/DE_BezierCurve2D.sql -O DE_BezierCurve2D.sql
+# load sql custom functions
+for i in *.sql; do
+psql -U $username -d $dbname -f $i
+done
+
+# Create PostGIS tables
+SQL=$(cat<<EOF
+-------------------------
+------- SQL BLOCK -------
+------------------------- 
+DROP TABLE IF EXISTS world_cities;
+CREATE TABLE world_cities (
+ogc_fid serial,
+wkb_geometry geometry(Point,4326),
+name character varying
+);
+
+INSERT INTO world cities SELECT NEXTVAL('world_cities_ogc_fid_seq'), ST_SetSRID(ST_MakePoint(-118.408530, 33.941625),4326), 'Los Angeles';
+INSERT INTO world cities SELECT NEXTVAL('world_cities_ogc_fid_seq'), ST_SetSRID(ST_MakePoint(151.175276, -33.939878),4326), 'Sydney';
+INSERT INTO world cities SELECT NEXTVAL('world_cities_ogc_fid_seq'), ST_SetSRID(ST_MakePoint(140.392882, 35.771978),4326), 'Tokyo';
+
+-------------------------
+EOF
+)
+echo "$SQL" # comment to suppress printing
+# execute SQL STATEMENT or comment # to skip
+psql -U $username -d $dbname -c "$SQL" ### alternatively, comment out line with single '#' to skip this step
+
+#######################################
+
+#######################################
+######### MAKE FLIGHT PATHS #########
+#######################################
+SQL=$(cat<<EOF
+-------------------------
+------- SQL BLOCK -------
+------------------------- 
+DROP TABLE IF EXISTS flight_paths;
+CREATE TABLE flight_paths (
+ogc_fid serial,
+origin text,
+destination text,
+wkb_geometry geometry(MultiLinestring, 4326)
+);
+
+WITH
+o AS (SELECT name, wkb_geometry FROM world_cities),
+d AS (SELECT name, wkb_geometry FROM world_cities),
+p AS (SELECT o.name as origin, d.name as destination, o.wkb_geometry as o_geom, d.wkb_geometry as d_geom FROM o,d WHERE o.name <> d.name)
+INSERT INTO flight_paths SELECT NEXTVAL('flight_paths_ogc_fid_seq'), p.origin, p.destination, DE_BezierCurve2D(p.o_geom, p.d_geom, 25, 1000, 30) FROM p;
+
+-------------------------
+EOF
+)
+echo "$SQL" # comment to suppress printing
+# execute SQL STATEMENT or comment # to skip
+psql -U $username -d $dbname -c "$SQL" ### alternatively, comment out line with single '#' to skip this step
+
+#######################################
+#########   Visualisation in QGIS  #########
+#######################################
+# Refer to this link for description of how to centre map in the Pacific.
+# http://gis.stackexchange.com/questions/70411/qgis-display-world-country-shape-files-centered-on-pacific-ocean-using-robinson
+#  In QGIS, you need to add a custom CRS entry. The following proj string centres the map in the Pacific ocean, specifically at 150 degrees west of the prime meridian.  This corresponds to a breakval of 30 degrees in the bezier curve function
+#  +proj=mill +lat_0=0 +lon_0=-150 +x_0=0 +y_0=0 +R_A +a=6371000 +b=6371000 +units=m +no_defs
+# In QGIS you need to set the Project CRS to your custom projection, making sure to enable 'on-the-fly' reprojection.
+
+SQL=$(cat<<EOF
+-------------------------
+------- SQL BLOCK -------
+------------------------- 
+DROP TABLE IF EXISTS world_borders_split;
+CREATE TABLE world_borders_split (
+fid integer,
+ogc_fid integer,
+name text,
+geometry geometry(Multipolygon, 4326)
+);
+
+INSERT INTO world_borders_split
+WITH r AS (SELECT 30 as val),
+s AS (SELECT ST_SetSRID(ST_MakePolygon(ST_MakeLine(ARRAY[ST_MakePoint(val-0.0000001,90), ST_MakePoint(val+0.0000001,90), ST_MakePoint(val+0.0000001,-90), ST_MakePoint(val-0.0000001,-90), ST_MakePoint(val-0.0000001,90)])),4326) as split_geom FROM r)
+SELECT row_number() over(), t1.ogc_fid, t1.name, ST_Multi(ST_Difference(t1.wkb_geometry, s.split_geom)) FROM world_borders t1, s;
+
+-------------------------
+EOF
+)
+echo "$SQL" # comment to suppress printing
+# execute SQL STATEMENT or comment # to skip
+psql -U $username -d $dbname -c "$SQL" ### alternatively, comment out line with single '#' to skip this step
+
+# Now you should be able to open up world borders split (centred on the Pacific Ocean), and the flight paths we generated above.
+
+# stop timer
+END_T1=$(date +%s)
+TOTAL_DIFF=$(( $END_T1 - $START_T1 ))
+echo "TOTAL SCRIPT TIME: $TOTAL_DIFF"
+# end of script
+#######################################
+
