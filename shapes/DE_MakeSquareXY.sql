b78c56ced9372286b0aad190bc62cfd84d21238b
markwynter <mark@dimensionaledge.com>
2015-07-26 00:19:18 +0000

    added

---
+++ shapes/DE_MakeSquareXY.sql
@@ -0,0 +1,26 @@
+---------------------------------------------------------------------------
+-- Code Desciption:
+---------------------------------------------------------------------------
+-- PostgreSQL/PostGIS custom function for generating a square polygon of a specified size
+-- Dependencies: nil
+-- Developed by: mark[at]dimensionaledge[dot]com
+-- Licence: GNU GPL version 3.0
+---------------------------------------------------------------------------
+-- Usage Example:
+---------------------------------------------------------------------------
+-- SELECT DE_MakeSquareXY(0,0,3577,10000);
+---------------------------------------------------------------------------
+
+CREATE OR REPLACE FUNCTION DE_MakeSquareXY(x FLOAT, y FLOAT, side FLOAT, srid FLOAT)
+RETURNS GEOMETRY
+AS $$
+SELECT ST_SetSRID(ST_MakePolygon(ST_MakeLine(
+ARRAY[
+				st_makepoint(x-0.5*side, y+0.5*side),
+				st_makepoint(x+0.5*side, y+0.5*side), 
+				st_makepoint(x+0.5*side, y-0.5*side), 
+				st_makepoint(x-0.5*side, y-0.5*side),
+				st_makepoint(x-0.5*side, y+0.5*side)
+				]
+)),srid);
+$$ LANGUAGE 'sql' IMMUTABLE STRICT;
